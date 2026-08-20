1ef35ef452c40035874821b28d42b9df365b0387
deliwalayash <yashdeliwala10@gmail.com>
2026-08-20 15:01:39 +0530

    google tag manager added

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,5 @@
 import { Sora } from "next/font/google";
+import Script from "next/script";
 import "../src/index.css";
 import "../src/App.css";
 import GoogleAnalytics from "../src/components/GoogleAnalytics";
@@ -145,7 +146,32 @@ const siteNavigationSchema = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Tag Manager */}
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PMD4SPD5');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
+      </head>
       <body className={sora.className}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PMD4SPD5"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <GoogleAnalytics />
         <script
           type="application/ld+json"
